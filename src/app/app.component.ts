27f592aa656c974ddfc75da395b4ae47aa9ad1c0
Bruno <bruno.penzar@posteo.de>
2018-09-10 12:28:47 +0200

    modify auto d3Json

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
-import {Component, OnInit} from '@angular/core';
-import {DataService} from "./services/data/data.service";
+import { Component, OnInit } from '@angular/core';
+import { DataService } from "./services/data/data.service";
 import { ConvertActionBindingResult } from '@angular/compiler/src/compiler_util/expression_converter';
 
 @Component({
@@ -18,11 +18,9 @@ export class AppComponent implements OnInit {
 
       let node_id = 0;
       // JSON --- d3
-      const d3Json = {"prefixes":[],"nodes":[],"links":[]};
-      
-
+      const d3Json = { "prefixes": [], "nodes": [], "links": [] };
       // --------------------------------------------------------------------------------------------
-      // noch die Keylist automatisieren...
+      // Keylist automatisieren...
       let keys = [];
       keys.push("$");
       keys.push("originatesFrom");
@@ -35,14 +33,14 @@ export class AppComponent implements OnInit {
 
       let keysNodeLink = [];
       keysNodeLink.push(["rdf:about"]);
-      keysNodeLink.push(["$","xmlns"]);
+      keysNodeLink.push(["$", "xmlns"]);
       keysNodeLink.push(["$", "rdf:resource"]);
       keysNodeLink.push(["$", "rdf:resource"]);
       keysNodeLink.push([]);
       keysNodeLink.push(["$", "rdf:resource"]);
       keysNodeLink.push(["$", "rdf:resource"]);
       keysNodeLink.push([]);
-      
+
       let keysNodeName = [];
       keysNodeName.push(["rdf:about"]);
       keysNodeName.push(["_"]);
@@ -58,115 +56,103 @@ export class AppComponent implements OnInit {
       content.map(about => {
 
         let sourceID = 0;
-        // IN GENERAL
-        for (let i = 0; i < keys.length; i++){
-          if (about[keys[i]] != null){
+        // search trought all keys 
+        for (let i = 0; i < keys.length; i++) {
+          if (about[keys[i]] != null) {
             if (about[keys[i]] instanceof Array) {
-              for (let l = 0; l < about[keys[i]].length; l++){
-                const node = {};
-                // Node_link from KeyMap
+              for (let l = 0; l < about[keys[i]].length; l++) {
+
+                let node = {};
+                // Node_link
                 node["link"] = about[keys[i]][l];
                 for (let j = 0; j < keysNodeLink[i].length; j++) {
-                  node["link"] = node["link"][keysNodeLink[i][j]];  
+                  node["link"] = node["link"][keysNodeLink[i][j]];
                 }
-                // Node_name from KeyMap   
+                // Node_name
                 node["name"] = about[keys[i]][l];
                 for (let j = 0; j < keysNodeName[i].length; j++) {
-                  node["name"] = node["name"][keysNodeName[i][j]];  
+                  node["name"] = node["name"][keysNodeName[i][j]];
                 }
-                // variables for the link
-                let typeID = keys[i];
-                if (keys[i] == "$"){
-                  sourceID = node_id;
-                }
-                // Check nodes + create node + create link
-                const vergleichsNode = d3Json.nodes[node_id];
-                checkDouble(node, d3Json, node_id, sourceID, typeID);
-                if (vergleichsNode != d3Json.nodes[node_id]){
+
+                // ADD NODE
+                // if node already exists
+                let check = 1;
+                d3Json.nodes.map(d3Node => {
+                  if (d3Node["name"] == node["name"]) {
+                    check = 0;
+                    node = d3Node;
+                  }
+                })
+                // if node does not exist
+                if (check) {
+                  node["id"] = node_id;
+                  d3Json.nodes[node_id] = node;
                   node_id++;
                 }
+                // sourceID
+                if (keys[i] == "$") {
+                  sourceID = node["id"];
+                }
+                // ADD LINK
+                addLinks(node, sourceID, d3Json, keys[i]);
               }
-            } 
+            }
+            // if about[keys[i]] no Array
             else {
-              const node = {};
-               // Node_link aus KeyMap
+
+              let node = {};
+              // Node_link
               node["link"] = about[keys[i]];
-              for (let j = 0; j <keysNodeLink[i].length; j++) {
-                node["link"] = node["link"][keysNodeLink[i][j]];  
+              for (let j = 0; j < keysNodeLink[i].length; j++) {
+                node["link"] = node["link"][keysNodeLink[i][j]];
               }
-              // Node_name aus KeyMap
+              // Node_name
               node["name"] = about[keys[i]];
-              for (let k = 0; k <keysNodeName[i].length; k++){
-                node["name"] = node["name"][keysNodeName[i][k]];  
+              for (let k = 0; k < keysNodeName[i].length; k++) {
+                node["name"] = node["name"][keysNodeName[i][k]];
               }
-              // variables for the link
-              let typeID = keys[i];
-              if (keys[i] == "$"){
-                sourceID = node_id;
-              }
-              // Check nodes + create node + create link
-              const vergleichsNode = d3Json.nodes[node_id];
-              checkDouble(node, d3Json, node_id, sourceID, typeID);
-              if (vergleichsNode != d3Json.nodes[node_id]){
-                node_id++;
-              }
+                // ADD NODE
+                // if node already exists
+                let check = 1;
+                d3Json.nodes.map(d3Node => {
+                  if (d3Node["name"] == node["name"]) {
+                    check = 0;
+                    node = d3Node;
+                  }
+                })
+                // if node does not exist
+                if (check) {
+                  node["id"] = node_id;
+                  d3Json.nodes[node_id] = node;
+                  node_id++;
+                }
+                // sourceID
+                if (keys[i] == "$") {
+                  sourceID = node["id"];
+                }
+                // ADD LINK  
+                addLinks(node, sourceID, d3Json, keys[i]);
             }
           }
         }
-      });   
-    
+      });
       console.log(d3Json);
-
-      // saveText( JSON.stringify(d3Json), "filename.json" );
-      
-    }, function(error) {
+    }, function (error) {
     });
-
-  }
-}
-
-// --------------------------------------------
-// Check if Node already in Nodes  + (if not) create node   + create link
-function checkDouble (node, d3Json, node_id, sourceID, typeID) {
-
-  let check = 1;
-  d3Json.nodes.map(element => {
-    if (element["name"] == node["name"]) {
-      // Check if nodes exists
-      check = 0;
-      // LINK -- V1
-      if (typeID != "$"){ 
-        let targetID;
-        targetID = element["id"];
-        const link = {};
-        link["source"] = sourceID;
-        link["target"] = targetID;
-        link["type"] = typeID;
-        d3Json.links.push(link);
-      }
-    }
-  })
-  if (check) {
-    node["id"] = node_id;
-    d3Json.nodes[node_id] = node;
-    // LINK -- V2
-    if (typeID != "$"){ 
-      let targetID;
-      targetID = node["id"];
-      const link = {};
-      link["source"] = sourceID;
-      link["target"] = targetID;
-      link["type"] = typeID;
-      d3Json.links.push(link);
-    }
   }
 }
 
 
-// --------------------------------------------
-// function saveText(text: string, filename: string) {
-//   const a = document.createElement('a');
-//   a.setAttribute('href', 'data:text/plain;charset=utf-u,' + encodeURIComponent(text));
-//   a.setAttribute('download', filename);
-//   a.click()
-// }
+function addLinks(node, sourceID, d3Json, key) {
+
+  let typeID = key;
+  if (typeID != "$") {
+    let targetID;
+    targetID = node["id"];
+    const link = {};
+    link["source"] = sourceID;
+    link["target"] = targetID;
+    link["type"] = typeID;
+    d3Json.links.push(link);
+  }
+}
