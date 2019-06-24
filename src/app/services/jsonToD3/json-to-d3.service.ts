c25100139824780d645470dc92f765b1531c9cb5
janwieners <jan@jan-wieners.de>
2019-06-24 12:18:15 +0200

    Change data.

--- src/app/services/jsonToD3/json-to-d3.service.ts
+++ src/app/services/jsonToD3/json-to-d3.service.ts
@@ -1,8 +1,8 @@
-import { Injectable } from '@angular/core';
-import { DataService } from "../data/data.service";
+import {Injectable} from '@angular/core';
+import {DataService} from '../data/data.service';
 
-import { HttpClient, HttpClientJsonpModule } from '@angular/common/http'; 
-import { Observable } from 'rxjs';
+import {HttpClient, HttpClientJsonpModule} from '@angular/common/http';
+import {Observable} from 'rxjs';
 
 
 @Injectable({
@@ -11,7 +11,7 @@ import { Observable } from 'rxjs';
 export class JsonToD3Service {
 
   public myD3PromiseObject: Promise<any>;
- 
+
 
   constructor(private dataService: DataService, private http: HttpClient) {
 
@@ -20,17 +20,17 @@ export class JsonToD3Service {
 
   createD3PromiseObject() {
 
-    let d3Json; 
-     
+    let d3Json;
+
     // myD3Data => zoneAwayPromise
-                                                          // RDF-XML Eingabedatei
+    // RDF-XML Eingabedatei
     this.myD3PromiseObject = this.dataService.xmlToJSON('./assets/wisski_test.xml').then(function (result) {
 
       console.log(result);
 
       // KeyList-Alternative
-      const keyList = createKeylist(result);      
-      
+      const keyList = createKeylist(result);
+
       // d3Json  
       d3Json = jsonToD3(keyList, result);
 
@@ -43,7 +43,7 @@ export class JsonToD3Service {
 
 
 function getJSON(file): Observable<any> {
-  HttpClientJsonpModule
+  HttpClientJsonpModule;
   return this.http.get(file);
 }
 
@@ -51,13 +51,13 @@ function getJSON(file): Observable<any> {
 // --------------------------------------------------------------------------------------------
 function createKeylist(result) {
 
-  const content = result["rdf:RDF"]["rdf:Description"];
+  const content = result['rdf:RDF']['rdf:Description'];
 
-  let keylist = [];  
+  let keylist = [];
   let raw1 = [];
   let raw2 = [];
   let raw3 = [];
-  
+
   // add keylist[] --- level-1
   // -------------------------
   content.map(object => {
@@ -78,34 +78,32 @@ function createKeylist(result) {
     Object.keys(object).map(key1 => {
 
       if (raw1[x1] == key1) {
- 
+
         let raw2_1 = [];
         Object.keys(object[key1]).map(key2 => {
-          
+
           addKeyToKeylist(key2, raw2_1);
         });
 
-        if(raw2_1[0] == "0") {
-          
+        if (raw2_1[0] == '0') {
+
           // IF ARRAY-0
           let bla = Object.keys(object[key1][raw2_1[0]]);
-          
-          if (bla[0] == "0") {
-            
-            raw2.push([null]);  
-          }
-          else {
+
+          if (bla[0] == '0') {
+
+            raw2.push([null]);
+          } else {
 
             raw2.push(bla);
           }
-        }
-        else {
+        } else {
 
           raw2.push(raw2_1);
         }
 
         x1++;
-      } 
+      }
     });
   });
   keylist.push(raw2);
@@ -123,43 +121,40 @@ function createKeylist(result) {
         let raw3_1 = [];
 
         Object.keys(object[key1]).map(key2 => {
-            
-            let raw3_2 = [];
-            Object.keys(object[key1][key2]).map(key3 =>{
 
-              // DEBUG
-              // -------
-              if (key3 == "$") {
-                Object.keys(object[key1][key2][key3]).map(key4 =>{
-                  addKeyToKeylist(key4, raw3_2);
-                });
-              }
-              else {
-                addKeyToKeylist(key3, raw3_2);
-              }
-              // -------
-            });
+          let raw3_2 = [];
+          Object.keys(object[key1][key2]).map(key3 => {
 
-            if(raw3_2[0] == "0") {
-
-              raw3_1.push([null]); 
+            // DEBUG
+            // -------
+            if (key3 == '$') {
+              Object.keys(object[key1][key2][key3]).map(key4 => {
+                addKeyToKeylist(key4, raw3_2);
+              });
+            } else {
+              addKeyToKeylist(key3, raw3_2);
             }
-            else {
-    
-              raw3_1.push(raw3_2);
-            }
+            // -------
+          });
+
+          if (raw3_2[0] == '0') {
+
+            raw3_1.push([null]);
+          } else {
+
+            raw3_1.push(raw3_2);
+          }
         });
 
-        if(raw3_1[0] == "0") {
+        if (raw3_1[0] == '0') {
 
-          raw3.push([null]);  
-        }
-        else {
+          raw3.push([null]);
+        } else {
 
           raw3.push(raw3_1);
         }
         x2++;
-      } 
+      }
     });
   });
   keylist.push(raw3);
@@ -184,13 +179,12 @@ function addKeyToKeylist(key, keylist) {
 }
 
 
-
 // --------------------------------------------------------------------------------------------
 // --------------------------------------------------------------------------------------------
-function jsonToD3(keyList, result) { 
+function jsonToD3(keyList, result) {
 
-  const content = result["rdf:RDF"]["rdf:Description"];
-  const d3Json_2 = { "prefixes": [], "nodes": [], "links": [] };
+  const content = result['rdf:RDF']['rdf:Description'];
+  const d3Json_2 = {'prefixes': [], 'nodes': [], 'links': []};
   let node_id_global = 0;
 
   content.map(object => {
@@ -200,45 +194,45 @@ function jsonToD3(keyList, result) {
 
     // ID OF THE FIRST ADDED NODE OF AN OBJECT GROUP
     let sourceID_2 = d3Json_2.nodes.length;
-    
+
     // Search Object for Keys (Level-1)
     for (let i = 0; i < x[0].length; i++) {
       // OBJECT[KEY]
       let o_1 = object[x[0][i]];
 
       // IF OBJECT NOT AN ARRAY
-      if (!Array.isArray(o_1)){
+      if (!Array.isArray(o_1)) {
 
         // IF OBJECT != UNDEFINED
         if (o_1 != undefined) {
 
-  
+
           // Search Object for Keys (Level-2)
           for (let j = 0; j < x[1][i].length; j++) {
             // OBJECT[KEY][KEY]
-            const o_2 = o_1[x[1][i][j]];           
+            const o_2 = o_1[x[1][i][j]];
             if (o_2 != undefined) {
-              
+
               // Search Object for Keys (Level-3)
-              for (let k = 0; k < x[2][i][j].length; k++) {  
+              for (let k = 0; k < x[2][i][j].length; k++) {
                 // OBJECT[KEY][KEY][KEY]              
-                let o_3 = o_2[x[2][i][j][k]];  
+                let o_3 = o_2[x[2][i][j][k]];
                 if (o_3 != undefined) {
 
                   // IF OBJECT-VALUE === STRING
                   // PUSH NODE -- LEVEL-3
-                  if(typeof o_3 === 'string'){
+                  if (typeof o_3 === 'string') {
 
                     // ADD NODE:
                     let node = {};
-                    node["type"] = x[0][i];
-                    node["label"] = o_3;
-                    node["id"] = node_id_global;
-                    node["color"] = 3;
+                    node['type'] = x[0][i];
+                    node['label'] = o_3;
+                    node['id'] = node_id_global;
+                    node['color'] = 3;
                     // Add Node
                     d3Json_2.nodes.push(node);
                     // Add Link
-                    addLink(node, sourceID_2, d3Json_2, node["type"]);
+                    addLink(node, sourceID_2, d3Json_2, node['type']);
                     node_id_global++;
                   }
                 }
@@ -246,44 +240,44 @@ function jsonToD3(keyList, result) {
 
               // IF OBJECT-VALUE === STRING
               // PUSH NODE -- LEVEL-2
-              if(typeof o_2 === 'string'){
+              if (typeof o_2 === 'string') {
 
                 // ADD NODE:
                 let node = {};
-                node["type"] = x[0][i];
-                node["label"] = object[x[0][i]][x[1][i][j]];
-                node["id"] = node_id_global;
-                node["color"] = 2;
+                node['type'] = x[0][i];
+                node['label'] = object[x[0][i]][x[1][i][j]];
+                node['id'] = node_id_global;
+                node['color'] = 2;
                 // Add Node
                 d3Json_2.nodes.push(node);
                 // Add Link
-                addLink(node, sourceID_2, d3Json_2, node["type"]);
+                addLink(node, sourceID_2, d3Json_2, node['type']);
                 node_id_global++;
 
                 // CHECK "RDF:DESCRIPTION" (= node["label"]) WITH GLOBAL_DATA_TABLE
                 // --------------------------------------------------------------
                 // --------------------------------------------------------------
                 let dataTable = require('../../../assets/dataTable.json');
-                
+
                 // go through all global-Objects
-                for(let o=0; o< dataTable["objects"].length; o++){
-                  
+                for (let o = 0; o < dataTable["objects"].length; o++) {
+
                   // Global-Object has "rdf:about"
-                  if (dataTable["objects"][o]["rdf:about"] != undefined){
+                  if (dataTable['objects'][o]['rdf:about'] != undefined) {
 
                     // When local and global Element have got the same "rdf:about"
-                    if (node["label"] == dataTable["objects"][o]["rdf:about"]){
-                      
+                    if (node['label'] == dataTable['objects'][o]['rdf:about']) {
+
                       // ADD NOTE WITH GLOBAL_IDENTIFIER
                       let node = {};
-                      node["type"] = "globalIdentifier";
-                      node["label"] = dataTable["objects"][o]["name"];
-                      node["id"] = node_id_global;
-                      node["color"] = 10;
+                      node['type'] = 'globalIdentifier';
+                      node['label'] = dataTable['objects'][o]['name'];
+                      node['id'] = node_id_global;
+                      node['color'] = 10;
                       // Add Node
                       d3Json_2.nodes.push(node);
                       // Add Link
-                      addLink(node, sourceID_2, d3Json_2, node["type"]);
+                      addLink(node, sourceID_2, d3Json_2, node['type']);
                       node_id_global++;
                     }
                   }
@@ -291,64 +285,64 @@ function jsonToD3(keyList, result) {
                 // --------------------------------------------------------------
                 // --------------------------------------------------------------
               }
-            }      
+            }
           }
 
           // IF OBJECT-VALUE === STRING
           // PUSH NODE -- LEVEL-1
-          if(typeof o_1 === 'string'){
-          
+          if (typeof o_1 === 'string') {
+
 
             // ADD NODE
             let node = {};
-            node["type"] = x[0][i];
-            node["label"] = o_1;
-            node["id"] = node_id_global;
-            node["color"] = 1;
+            node['type'] = x[0][i];
+            node['label'] = o_1;
+            node['id'] = node_id_global;
+            node['color'] = 1;
             // Add Node
             d3Json_2.nodes.push(node);
             // Add Link
-            addLink(node, sourceID_2, d3Json_2, node["type"]);
+            addLink(node, sourceID_2, d3Json_2, node['type']);
             node_id_global++;
           }
-  
-  
-        }      
+
+
+        }
       }
       // IF OBJECT IS AN ARRAY
       else {
 
         // Loop through Array Objects
-        for (let v = 0; v < o_1.length; v++){
+        for (let v = 0; v < o_1.length; v++) {
           // OBJECT IN ARRAY
           const o_1_1 = o_1[v];
-          
+
           // Search Object for Keys (Level-2)
           for (let j = 0; j < x[1][i].length; j++) {
             // OBJECT[KEY][KEY]
-            const o_2 = o_1_1[x[1][i][j]];           
+            const o_2 = o_1_1[x[1][i][j]];
             if (o_2 != undefined) {
-              
+
               // Search Object for Keys (Level-3)
-              for (let k = 0; k < x[2][i][j].length; k++) {  
+              for (let k = 0; k < x[2][i][j].length; k++) {
                 // OBJECT[KEY][KEY][KEY]              
-                let o_3 = o_2[x[2][i][j][k]];  
+                let o_3 = o_2[x[2][i][j][k]];
                 if (o_3 != undefined) {
 
                   // IF OBJECT-VALUE === STRING
                   // PUSH NODE -- LEVEL-3
-                  if(typeof o_3 === 'string'){
+                  if (typeof o_3 === 'string') {
 
                     // ADD NODE:
                     let node = {};
-                    node["type"] = x[0][i];
-                    node["label"] = o_3;
-                    node["id"] = node_id_global;
-                    node["color"] = 3;
+                    node['type'] = x[0][i];
+                    node['label'] = o_3;
+                    node['id'] = node_id_global;
+                    node['color'] = 3;
                     // Add Node
                     d3Json_2.nodes.push(node);
                     // Add Link
-                    addLink(node, sourceID_2, d3Json_2, node["type"]);
+                    addLink(node, sourceID_2, d3Json_2, node['type']);
                     node_id_global++;
                   }
                 }
@@ -356,20 +350,20 @@ function jsonToD3(keyList, result) {
 
               // IF OBJECT-VALUE === STRING
               // PUSH NODE -- LEVEL-3
-              if(typeof o_2 === 'string'){
+              if (typeof o_2 === 'string') {
 
                 // ADD NODE:
                 let node = {};
-                node["type"] = x[0][i];
-                node["label"] = o_2;
-                node["id"] = node_id_global;
-                node["color"] = 2;
+                node['type'] = x[0][i];
+                node['label'] = o_2;
+                node['id'] = node_id_global;
+                node['color'] = 2;
                 // Add Node
                 d3Json_2.nodes.push(node);
                 // Add Link
                 addLink(node, sourceID_2, d3Json_2, x[0][i]);
                 node_id_global++;
-              }              
+              }
             }
           }
         }
@@ -384,14 +378,14 @@ function addLink(node, sourceID, d3Json, key) {
 
   let typeID = key;
 
-  if (typeID != "$") {
+  if (typeID != '$') {
 
     let targetID;
-    targetID = node["id"];
+    targetID = node['id'];
     const link = {};
-    link["source"] = sourceID;
-    link["target"] = targetID;
-    link["type"] = typeID;
+    link['source'] = sourceID;
+    link['target'] = targetID;
+    link['type'] = typeID;
     d3Json.links.push(link);
   }
 }
